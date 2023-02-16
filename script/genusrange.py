243dfa592323806568e967a674cee2061bc672cc
Simon <grognardsimon@gmail.com>
2023-02-16 15:10:05 +0100

    ajout species range et genus range

--- script/genusrange.py
+++ script/genusrange.py
@@ -4,9 +4,9 @@ import numpy as np
 import math
 
 def insertOrder(data, cursor, conn) :
-    toinsert = data["Species_range"].values.tolist()
-    duplicationquery =  """SELECT MAX(id_speciesrange)
-                            FROM SpeciesRange"""
+    toinsert = data["Genus_range"].values.tolist()
+    duplicationquery =  """SELECT MAX(id_genusrange)
+                            FROM GenusRange"""
     cursor.execute(duplicationquery)
     result = cursor.fetchall()
     Count = 1
@@ -27,12 +27,12 @@ def insertOrder(data, cursor, conn) :
             print("start", start)
             print("end ", end)
             duplicationquery =  """SELECT *
-                                    FROM SpeciesRange 
+                                    FROM GenusRange 
                                     WHERE range_begin = "{}" and range_end= "{}" """.format(start, end) 
             cursor.execute(duplicationquery)
             if cursor.fetchall() == [] :
-                insertquery = """INSERT INTO SpeciesRange
-                                (id_speciesrange, range_begin, range_end) 
+                insertquery = """INSERT INTO GenusRange
+                                (id_genusrange, range_begin, range_end) 
                                 VALUES 
                                 ({},"{}", "{}")""".format(Count, start, end)
                                 #print(insertquery)
