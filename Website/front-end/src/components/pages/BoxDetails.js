0a24bb9614e90525e5b6411a572a5027449aa77f
jschot <jean.schot@student.uclouvain.be>
2023-03-20 12:46:56 +0100

    preparation BoxDetails&InsectDetails

--- Website/front-end/src/components/pages/BoxDetails.js
+++ Website/front-end/src/components/pages/BoxDetails.js
@@ -5,146 +5,277 @@ import './Details.css';
 import {Navigate} from 'react-router-dom';
 
 class BoxDetails extends React.Component {
-    constructor (props) {
-      super(props)
-      this.state = {
-        order: 'Test1',
-        suborder: 'Test2',
-        genus: 'NULL',
-        subgenus: 'NULL',
-        family: 'NULL',
-        subfamily: 'NULL',
-        species: 'NULL',
-        subspecies: 'NULL',
-        tribus: 'NULL',
-        subtribus: 'NULL',
-        loaner: 'NULL',
-        rangebegin: 'NULL',
-        rangeend: 'NULL',
-        collection: 'NULL',
-      }
+  constructor (props) {
+    super(props)
+    this.state = {
+      order: 'NULL',
+      suborder: 'NULL',
+      genus: 'NULL',
+      subgenus: 'NULL',
+      family: 'NULL',
+      subfamily: 'NULL',
+      species: 'NULL',
+      subspecies: 'NULL',
+      tribus: 'NULL',
+      loaner: 'NULL',
+      rangebegin: 'NULL',
+      rangeend: 'NULL',
+      collection: 'NULL',
+      orderlist: [],
+      suborderlist: [],
+      genuslist: [],
+      subgenuslist: [],
+      familylist: [],
+      subfamilylist: [],
+      specieslist: [],
+      subspecieslist: [],
+      tribuslist: [],
+      loanerlist: [],
+      rangebeginlist: [],
+      rangeendlist: [],
+      collectionlist: [],
     }
+  }
 
-    render() {
-      return (
+  OrderChange = (event) => {
+    this.setState({order: event.target.value})
+  }
+
+  render() {
+    return (
+      <>
+      {this.props.isAuthenticated() ? 
+        (
+        //Admin's Version
         <>
-        {this.props.isAuthenticated() ? 
-          (
-          <>
-            <Navbar />
-            <div className="container">
-                <div>
-                  <p className="title">Order</p>
-                  {(this.state.order) ?
-                    (<p>{this.state.order}</p>)
-                    :
-                    (<></>)
-                  }
-                </div>
-                <div>
-                  <p className="title">Suborder</p>
-                  {(this.state.suborder) ?
-                    (<p>{this.state.suborder}</p>)
-                    :
-                    (<></>)
-                  }
-                </div>
-                <div>
-                  <p className="title">Genus</p>
-                  {(this.state.genus) ?
-                    (<p>{this.state.genus}</p>)
-                    :
-                    (<></>)
-                  }
-                </div>
-                <div>
-                  <p  className="title">Subgenus</p>
-                  {(this.state.subgenus) ?
-                    (<p>{this.state.subgenus}</p>)
-                    :
-                    (<></>)
-                  }
-                </div>
-                <div>
-                  <p className="title">Family</p>
-                  {(this.state.family) ?
-                    (<p>{this.state.family}</p>)
-                    :
-                    (<></>)
-                  }
-                </div>
-                <div>
-                  <p className="title">Subfamily</p>
-                  {(this.state.subfamily) ?
-                    (<p>{this.state.subfamily}</p>)
-                    :
-                    (<></>)
-                  }
-                </div>
-                <div>
-                  <p className="title">Species</p>
-                  {(this.state.species) ?
-                    (<p>{this.state.species}</p>)
-                    :
-                    (<></>)
-                  }
-                </div>
-                <div>
-                  <p className="title">Subspecies</p>
-                  {(this.state.subspecies) ?
-                    (<p>{this.state.subspecies}</p>)
-                    :
-                    (<></>)
-                  }
-                </div>
-                <div>
-                  <p className="title">Tribus</p>
-                  {(this.state.tribus) ?
-                    (<p>{this.state.tribus}</p>)
-                    :
-                    (<></>)
-                  }
-                </div>
-                <div>
-                  <p className="title">Subtribus</p>
-                  {(this.state.subtribus) ?
-                    (<p>{this.state.subtribus}</p>)
-                    :
-                    (<></>)
-                  }
-                </div>
-                <div>
-                  <p className="title">Loaner</p>
-                  {(this.state.loaner) ?
-                    (<p>{this.state.loaner}</p>)
-                    :
-                    (<></>)
-                  }
-                </div>
-                <div>
-                  <p className="title">Range</p>
-                  {(this.state.rangebegin) ?
-                    (<p>{this.state.rangebegin}-{this.state.rangeend}</p>)
-                    :
-                    (<></>)
-                  }
-                </div>
-                <div>
-                  <p className="title">Collection</p>
-                  {(this.state.rangebegin) ?
-                    (<p>{this.state.collection}</p>)
-                    :
-                    (<></>)
-                  }
-                </div>
-            </div>
-          </>
-          ) : (
-            <Navigate to='/sign-in' />
-          )
-        }
+          <Navbar />
+          <div className="container">
+              <div>
+                <p className="title">Order</p>
+                <FormControl variant="standard" sx={{ m: 1, minWidth: 120 }}>
+                  <InputLabel id="demo-simple-select-label">Order</InputLabel>
+                  <Select
+                    labelId="order-label"
+                    id="order-select"
+                    value={this.state.order}
+                    label="Order"
+                    onChange={this.OrderChange}
+                  >
+                    {this.state.orderlist.map((data) => <MenuItem value={data.name}>{data.name}</MenuItem>)}
+                    <MenuItem value={"Hymenoptera"}>Hymenoptera</MenuItem>
+                    <MenuItem value={"Lepidoptera"}>Lepidoptera</MenuItem>
+                  </Select>
+                </FormControl>
+              </div>
+              <div>
+                <p className="title">Suborder</p>
+                <FormControl variant="standard" sx={{ m: 1, minWidth: 120 }}>
+                  <InputLabel id="demo-simple-select-label">Order</InputLabel>
+                  <Select
+                    labelId="order-label"
+                    id="order-select"
+                    value={this.state.order}
+                    label="Order"
+                    onChange={this.OrderChange}
+                  >
+                    <MenuItem value='NULL'>
+                      <em>None</em>
+                    </MenuItem>
+                    {this.state.orderlist.map((data) => <MenuItem value={data.name}>{data.name}</MenuItem>)}
+                    <MenuItem value={"Hymenoptera"}>Hymenoptera</MenuItem>
+                    <MenuItem value={"Lepidoptera"}>Lepidoptera</MenuItem>
+                  </Select>
+                </FormControl>
+              </div>
+              <div>
+                <p className="title">Genus</p>
+                {(this.state.genus) ?
+                  (<p>{this.state.genus}</p>)
+                  :
+                  (<></>)
+                }
+              </div>
+              <div>
+                <p  className="title">Subgenus</p>
+                {(this.state.subgenus) ?
+                  (<p>{this.state.subgenus}</p>)
+                  :
+                  (<></>)
+                }
+              </div>
+              <div>
+                <p className="title">Family</p>
+                {(this.state.family) ?
+                  (<p>{this.state.family}</p>)
+                  :
+                  (<></>)
+                }
+              </div>
+              <div>
+                <p className="title">Subfamily</p>
+                {(this.state.subfamily) ?
+                  (<p>{this.state.subfamily}</p>)
+                  :
+                  (<></>)
+                }
+              </div>
+              <div>
+                <p className="title">Species</p>
+                {(this.state.species) ?
+                  (<p>{this.state.species}</p>)
+                  :
+                  (<></>)
+                }
+              </div>
+              <div>
+                <p className="title">Subspecies</p>
+                {(this.state.subspecies) ?
+                  (<p>{this.state.subspecies}</p>)
+                  :
+                  (<></>)
+                }
+              </div>
+              <div>
+                <p className="title">Tribus</p>
+                {(this.state.tribus) ?
+                  (<p>{this.state.tribus}</p>)
+                  :
+                  (<></>)
+                }
+              </div>
+              <div>
+                <p className="title">Loaner</p>
+                {(this.state.loaner) ?
+                  (<p>{this.state.loaner}</p>)
+                  :
+                  (<></>)
+                }
+              </div>
+              <div>
+                <p className="title">Range</p>
+                {(this.state.rangebegin) ?
+                  (<p>{this.state.rangebegin}-{this.state.rangeend}</p>)
+                  :
+                  (<></>)
+                }
+              </div>
+              <div>
+                <p className="title">Collection</p>
+                {(this.state.rangebegin) ?
+                  (<p>{this.state.collection}</p>)
+                  :
+                  (<></>)
+                }
+              </div>
+          </div>
         </>
-    )}
+      ) : (
+        //User's Version
+        <>
+        <Navbar />
+        <div className="container">
+            <div>
+              <p className="title">Order</p>
+              {(this.state.order) === 'NULL' ?
+                (<></>)
+                :
+                (<p>{this.state.order}</p>)
+              }
+            </div>
+            <div>
+              <p className="title">Suborder</p>
+              {(this.state.suborder) === 'NULL' ?
+                (<></>)
+                :
+                (<p>{this.state.suborder}</p>)
+              }
+            </div>
+            <div>
+              <p className="title">Genus</p>
+              {(this.state.genus) === 'NULL' ?
+                (<></>)
+                :
+                (<p>{this.state.genus}</p>)
+              }
+            </div>
+            <div>
+              <p  className="title">Subgenus</p>
+              {(this.state.subgenus) === 'NULL'? 
+                (<></>)
+                :
+                (<p>{this.state.subgenus}</p>)
+              }
+            </div>
+            <div>
+              <p className="title">Family</p>
+              {(this.state.family) === 'NULL' ?
+                (<></>)
+                :
+                (<p>{this.state.family}</p>)
+              }
+            </div>
+            <div>
+              <p className="title">Subfamily</p>
+              {(this.state.subfamily) === 'NULL' ?
+                (<></>)
+                :
+                (<p>{this.state.subfamily}</p>)
+              }
+            </div>
+            <div>
+              <p className="title">Species</p>
+              {(this.state.species) === 'NULL' ?
+                (<></>)
+                :
+                (<p>{this.state.species}</p>)
+              }
+            </div>
+            <div>
+              <p className="title">Subspecies</p>
+              {(this.state.subspecies) === 'NULL' ?
+                (<></>)
+                :
+                (<p>{this.state.subspecies}</p>)
+              }
+            </div>
+            <div>
+              <p className="title">Tribus</p>
+              {(this.state.tribus) === 'NULL' ?
+                (<></>)
+                :
+                (<p>{this.state.tribus}</p>)
+              }
+            </div>
+            <div>
+              <p className="title">Loaner</p>
+              {(this.state.loaner) === 'NULL' ?
+                (<></>)
+                :
+                (<p>{this.state.loaner}</p>)
+              }
+            </div>
+            <div>
+              <p className="title">Range</p>
+              {(this.state.rangebegin) === 'NULL' ?
+                (<></>)
+                :
+                (<p>{this.state.rangebegin}-{this.state.rangeend}</p>)
+              }
+            </div>
+            <div>
+              <p className="title">Collection</p>
+              {(this.state.collection) === 'NULL' ?
+                (<></>)
+                :
+                (<p>{this.state.collection}</p>)
+              }
+            </div>
+        </div>
+      </>
+      )
+    }
+    </>
+  )}
 }
 
 export default BoxDetails;
--- Website/front-end/src/components/pages/InsectDetails.js
+++ Website/front-end/src/components/pages/InsectDetails.js
@@ -1,8 +1,10 @@
 import * as React from 'react';
 import Navbar from '../Navbar';
 import './Details.css';
-
-import {Navigate} from 'react-router-dom';
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import FormControl from '@mui/material/FormControl';
+import Select from '@mui/material/Select';
 
 class InsectDetails extends React.Component {
     constructor (props) {
@@ -15,14 +17,30 @@ class InsectDetails extends React.Component {
         family: 'NULL',
         subfamily: 'NULL',
         species: 'NULL',
-        subspecies: 'Test',
+        subspecies: 'NULL',
         tribus: 'NULL',
-        subtribus: 'NULL',
         loaner: 'NULL',
         rangebegin: 'NULL',
         rangeend: 'NULL',
         collection: 'NULL',
+        orderlist: [],
+        suborderlist: [],
+        genuslist: [],
+        subgenuslist: [],
+        familylist: [],
+        subfamilylist: [],
+        specieslist: [],
+        subspecieslist: [],
+        tribuslist: [],
+        loanerlist: [],
+        rangebeginlist: [],
+        rangeendlist: [],
+        collectionlist: [],
       }
+    }
+
+    OrderChange = (event) => {
+      this.setState({order: event.target.value})
     }
 
     render() {
@@ -30,24 +48,46 @@ class InsectDetails extends React.Component {
         <>
         {this.props.isAuthenticated() ? 
           (
+          //Admin's Version
           <>
             <Navbar />
             <div className="container">
                 <div>
                   <p className="title">Order</p>
-                  {(this.state.order) ?
-                    (<p>{this.state.order}</p>)
-                    :
-                    (<></>)
-                  }
+                  <FormControl variant="standard" sx={{ m: 1, minWidth: 120 }}>
+                    <InputLabel id="demo-simple-select-label">Order</InputLabel>
+                    <Select
+                      labelId="order-label"
+                      id="order-select"
+                      value={this.state.order}
+                      label="Order"
+                      onChange={this.OrderChange}
+                    >
+                      {this.state.orderlist.map((data) => <MenuItem value={data.name}>{data.name}</MenuItem>)}
+                      <MenuItem value={"Hymenoptera"}>Hymenoptera</MenuItem>
+                      <MenuItem value={"Lepidoptera"}>Lepidoptera</MenuItem>
+                    </Select>
+                  </FormControl>
                 </div>
                 <div>
                   <p className="title">Suborder</p>
-                  {(this.state.suborder) ?
-                    (<p>{this.state.suborder}</p>)
-                    :
-                    (<></>)
-                  }
+                  <FormControl variant="standard" sx={{ m: 1, minWidth: 120 }}>
+                    <InputLabel id="demo-simple-select-label">Order</InputLabel>
+                    <Select
+                      labelId="order-label"
+                      id="order-select"
+                      value={this.state.order}
+                      label="Order"
+                      onChange={this.OrderChange}
+                    >
+                      <MenuItem value='NULL'>
+                        <em>None</em>
+                      </MenuItem>
+                      {this.state.orderlist.map((data) => <MenuItem value={data.name}>{data.name}</MenuItem>)}
+                      <MenuItem value={"Hymenoptera"}>Hymenoptera</MenuItem>
+                      <MenuItem value={"Lepidoptera"}>Lepidoptera</MenuItem>
+                    </Select>
+                  </FormControl>
                 </div>
                 <div>
                   <p className="title">Genus</p>
@@ -106,14 +146,6 @@ class InsectDetails extends React.Component {
                   }
                 </div>
                 <div>
-                  <p className="title">Subtribus</p>
-                  {(this.state.subtribus) ?
-                    (<p>{this.state.subtribus}</p>)
-                    :
-                    (<></>)
-                  }
-                </div>
-                <div>
                   <p className="title">Loaner</p>
                   {(this.state.loaner) ?
                     (<p>{this.state.loaner}</p>)
@@ -140,7 +172,108 @@ class InsectDetails extends React.Component {
             </div>
           </>
         ) : (
-          <Navigate to='/sign-in' />
+          //User's Version
+          <>
+          <Navbar />
+          <div className="container">
+              <div>
+                <p className="title">Order</p>
+                {(this.state.order) === 'NULL' ?
+                  (<></>)
+                  :
+                  (<p>{this.state.order}</p>)
+                }
+              </div>
+              <div>
+                <p className="title">Suborder</p>
+                {(this.state.suborder) === 'NULL' ?
+                  (<></>)
+                  :
+                  (<p>{this.state.suborder}</p>)
+                }
+              </div>
+              <div>
+                <p className="title">Genus</p>
+                {(this.state.genus) === 'NULL' ?
+                  (<></>)
+                  :
+                  (<p>{this.state.genus}</p>)
+                }
+              </div>
+              <div>
+                <p  className="title">Subgenus</p>
+                {(this.state.subgenus) === 'NULL'? 
+                  (<></>)
+                  :
+                  (<p>{this.state.subgenus}</p>)
+                }
+              </div>
+              <div>
+                <p className="title">Family</p>
+                {(this.state.family) === 'NULL' ?
+                  (<></>)
+                  :
+                  (<p>{this.state.family}</p>)
+                }
+              </div>
+              <div>
+                <p className="title">Subfamily</p>
+                {(this.state.subfamily) === 'NULL' ?
+                  (<></>)
+                  :
+                  (<p>{this.state.subfamily}</p>)
+                }
+              </div>
+              <div>
+                <p className="title">Species</p>
+                {(this.state.species) === 'NULL' ?
+                  (<></>)
+                  :
+                  (<p>{this.state.species}</p>)
+                }
+              </div>
+              <div>
+                <p className="title">Subspecies</p>
+                {(this.state.subspecies) === 'NULL' ?
+                  (<></>)
+                  :
+                  (<p>{this.state.subspecies}</p>)
+                }
+              </div>
+              <div>
+                <p className="title">Tribus</p>
+                {(this.state.tribus) === 'NULL' ?
+                  (<></>)
+                  :
+                  (<p>{this.state.tribus}</p>)
+                }
+              </div>
+              <div>
+                <p className="title">Loaner</p>
+                {(this.state.loaner) === 'NULL' ?
+                  (<></>)
+                  :
+                  (<p>{this.state.loaner}</p>)
+                }
+              </div>
+              <div>
+                <p className="title">Range</p>
+                {(this.state.rangebegin) === 'NULL' ?
+                  (<></>)
+                  :
+                  (<p>{this.state.rangebegin}-{this.state.rangeend}</p>)
+                }
+              </div>
+              <div>
+                <p className="title">Collection</p>
+                {(this.state.collection) === 'NULL' ?
+                  (<></>)
+                  :
+                  (<p>{this.state.collection}</p>)
+                }
+              </div>
+          </div>
+        </>
         )
       }
       </>
